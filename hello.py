4409a410a551afef0e7f02e0dffddfb132959805
Mahdi Bin Ferdaus <144200594+mahdibinferdaus@users.noreply.github.com>
2025-01-11 14:32:15 +0600

    hello.py

--- hello.py
+++ hello.py
@@ -1,36 +1,44 @@
-import cv2
-from flask import Flask, Response
+from flask import Flask, render_template, request, jsonify
 
-# Initialize Flask app
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
 app = Flask(__name__)
 
-# Initialize webcam (0 is usually the default webcam on Mac)
-cap = cv2.VideoCapture(0)
+@app.route("/")
+def index():
+    return render_template('chat.html')
 
 
-def generate_frames():
-    while True:
-        # Capture frame-by-frame from the webcam
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Encode the frame to JPEG format
-            ret, buffer = cv2.imencode(".jpg", frame)
-            frame = buffer.tobytes()
-
-            # Yield the frame in byte format as multipart
-            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
 
 
-# Route to serve the video stream
-@app.route("/video_feed")
-def video_feed():
-    return Response(
-        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
+def get_Chat_response(text):
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 
-# Start the Flask app
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run()
